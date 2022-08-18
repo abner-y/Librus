3f162dd8ffaaabeb45c5250545721f58f4c77fa7
Abner <abnergithub@gmail.com>
2022-08-18 13:52:54 -0300

    Inserting project on git

---
+++ main.py
@@ -0,0 +1,53 @@
+from kivy.app import App
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+import mysql.connector
+
+Builder.load_file('primeiraPagina.kv')
+
+class PrimeiraTela(Screen):
+    text_input_str = StringProperty()
+
+    def on_text_validate(self, widget):
+        self.text_input_str = widget.text
+        print(self.text_input_str)
+
+    def criarCaso(self):
+        print('criar caso')
+        print(self.ids)
+
+    def painelInvestigacao(self):
+        print('Painel')
+
+
+class SegundaTela(Screen):
+    pass
+
+
+class TerceiraTela(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    id_pagina = NumericProperty()
+    print(f'{id_pagina}')
+
+class EstruturaLivro(RelativeLayout):
+    pass
+
+
+
+#class LibrusApp(App):
+ #   def on_start(self):
+  #      print(self.root.ids)
+
+
+class MainApp(App):
+    def on_build(self):
+        pass
+
+
+
+MainApp().run()
---
+++ primeiraPagina.py
@@ -0,0 +1,74 @@
+import mysql
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.graphics import Rectangle
+from kivy.properties import ObjectProperty, Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+
+
+class PrimeiraPagina(RelativeLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_once(self.post_init, 0)
+
+    def post_init(self, *args):
+        print(f'{self.ids}< - primeira pagina -> {self.ids}')
+
+
+    def on_touch_down(self, touch):
+        if self.opacity == 0:
+            return False
+        return super(RelativeLayout, self).on_touch_down(touch)
+
+class CaixaCasos(BoxLayout):
+    pass
+
+
+class ViewCasos:
+    pass
+
+
+
+
+
+class ViewCasos(ScrollView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_once(self.post_init, 0)
+
+
+    def post_init(self, *args):
+        print(f'{self.ids}< - ids viewCasos -> {self.ids}')
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="123456",
+            database="librus",
+        )
+
+        c = mydb.cursor()
+        c.execute("SELECT nomeCaso, descricao, idcasos FROM casos")
+        resultados = c.fetchall()
+        print(resultados)
+
+        c.close()
+        mydb.close()
+        for resultado in resultados:
+            self.submit = Button(text = f'{resultado[0]}' if len(resultado[0]) < 15 else f'{resultado[0][0:15]}...', font_size = 30, size_hint_y = None, height = 50, color = (0, 0, 0), background_color = (1, 1, 1, 0.1))
+            self.submit.bind(on_press = self.pressed)
+            self.submit.ids['id'] = resultado[2]
+            self.ids.box.add_widget(self.submit)
+            self.ids.box.add_widget(TextInput(text=f'{resultado[1][0:100]}...', font_size=15, size_hint_y = None, height = 70, background_color = (0, 0, 0, 0), disabled = True, disabled_foreground_color = (0, 0, 0, 1)))
+
+    def pressed(self, instance):
+        id = instance.ids
+        app = App.get_running_app()
+        app.root.current = 'segunda'
+        app.root.transition.direction = 'up'
+        app.root.id_pagina = id['id']
+
